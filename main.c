36e1d68d14676b252c4bbdc9d7f768c70f6f473f
YoungkwanCho <dudrhks1009@naver.com>
2022-04-12 14:55:45 +0900

    filesave, fileload, filesearch mode added. Ver1

--- main.c
+++ main.c
@@ -3,25 +3,28 @@
 
 int selectProduct(){
     int Product;
-    printf("\n****** 제품 판매 ******\n1. 제품 조회\n2. 제품 추가\n3. 제품 수정\n4. 제품 삭제\n0. 종료\n");
+    printf("\n****** 제품 판매 ******\n1. 제품 조회\n2. 제품 추가\n3. 제품 수정\n4. 제품 삭제\n5. 제품 저장\n6. 제품이름 검색\n7. 제품가격 검색\n8. 제품 배송방법 검색\n0. 종료\n");
     printf("\n=> 원하는 메뉴는? ");
     scanf("%d", &Product);
     return Product;
 }
 
 int main(void){
-    product sp[30];
+	int n=50;
+    product sp[n];
     int count = 0, a;
     int index=0;
 
 	#ifdef DEBUG
 	printf("DEBUG MODE ON:\n");
 	#endif
+    count = loadData(sp, n);
+    index = count;
     while (1){
         a = selectProduct();
         if (a == 0) break;
-        if (a == 1 || a == 3 || a == 4) {
-            if (count==0) continue;
+        if ( a == 1 || a == 3 || a == 4 || a == 5 || a == 6 || a == 7 || a == 8) {
+           if (count==0) continue;
         }
         if (a == 1) {
             listProduct(sp, index);
@@ -60,6 +63,18 @@ int main(void){
 			#endif
                 }
             }
+        }
+	else if (a==5) {
+            saveData(sp, index);
+        }
+        else if (a==6) {
+            searchProductName(sp, count);
+        }
+        else if (a==7) {
+            searchProductPrice(sp, count);
+        }
+        else if (a==8) {
+            searchProductShipping(sp, count);
         }
     }
     printf("종료됨!\n");
