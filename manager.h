6d248281eed1ff22a4cc5171751330746ffef3ba
YoungkwanCho <dudrhks1009@naver.com>
2022-04-12 14:54:51 +0900

    manager.h modified

--- manager.h
+++ manager.h
@@ -2,3 +2,9 @@
 
 int selectDataNo (product *s, int count); // 특정 제품의 데이터 위치를 알아내기 위한 함수
 void listProduct(product *s, int count);   // 현재있는 제품 알려주는 함수(삭제된 값은 스킵함)
+
+void saveData(product *s, int count);
+int loadData(product *s, int number);
+void searchProductName(product *s, int count);
+void searchProductShipping(product *s, int count);
+void searchProductPrice(product *s, int count);
